ad841e0873bd3a29e0a24a34525b48ed61beab36
pablofon <paulete874@gmail.com>
2024-10-07 17:54:04 +0200

    fghjkl

---
+++ BaldosasRepoAndroid/Assets/BaldosasRepo_root/Scripts/Managers/LoadManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadManager : MonoBehaviour
+{
+    public void SceneLoader(int sceneToLoad)
+    {
+        //GameManager.Instance.GameResetStatus(); //Crear en el GameManager si es necesario
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void MainMenuSceneLoader(int sceneToLoad)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("saliendo de Juego");
+        Application.Quit();
+    }
+}
